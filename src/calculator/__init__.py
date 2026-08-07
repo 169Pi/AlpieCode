dc85fe84a5add1d0011f68ed8b8ce489822d86dc
AmanS3109 <singhaman2122@ternaengg.ac.in>
2026-08-07 07:03:01 +0000

    checkpoint: turn 7

---
+++ src/calculator/__init__.py
@@ -0,0 +1,8 @@
+"""
+Calculator package - A CLI calculator with comprehensive operations.
+"""
+
+from .calculator import Calculator, calculate
+
+__all__ = ['Calculator', 'calculate']
+__version__ = '1.0.0'
